a283badfe8d42c8c56783e71706297d359c0d38f
= <=>
2022-07-20 11:36:07 +0700

    Lutfi

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        JobConf conf = new JobConf(org.example.ramenbrand.App.class);
+        conf.setJobName("Ramen Produced By Country");
+
+
+        conf.setMapperClass(RamenReviewedByCountryMapper.class);
+
+        conf.setMapOutputKeyClass(Text.class);
+        conf.setMapOutputValueClass(Text.class);
+
+        conf.setReducerClass(RamenReviewedByCountryReducer.class);
+
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        try {
+            JobClient.runJob(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/ramenbrand/App.java
@@ -0,0 +1,39 @@
+package org.example.ramenbrand;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        JobConf conf = new JobConf(App.class);
+        conf.setJobName("Ramen Brand Variety");
+
+        conf.setMapperClass(RamenBrandVarietyMapper.class);
+
+        conf.setMapOutputKeyClass(Text.class);
+        conf.setMapOutputValueClass(Text.class);
+
+        conf.setReducerClass(RamenBrandVarietyReducer.class);
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        try {
+            JobClient.runJob(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
